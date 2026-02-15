b65ae2dd22f1676640d7e57749233609f22330e3
Siciliano <anthonyezedinachi@gmail.com>
2026-02-15 01:25:50 +0100

    Update home.tsx

--- frontend/app/home.tsx
+++ frontend/app/home.tsx
@@ -8,7 +8,7 @@ import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import Constants from 'expo-constants';
-import { getPendingCount, processQueue } from '../../utils/offlineQueue';
+import { getPendingCount, processQueue } from '../utils/offlineQueue';
 import { getAuthToken, clearAuthData, getUserMetadata } from '../utils/auth';
 
 const BACKEND_URL = Constants.expoConfig?.extra?.backendUrl || process.env.EXPO_PUBLIC_BACKEND_URL;
